6ce1dbdf01d78bd8e86d2b5e893d3cddbe0a9fc4
talamoig <ivano.talamo@gmail.com>
2015-04-10 18:46:10 +0200

    updated to release 1.1-1 and minor indentation/spacing changes

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,19 +1,18 @@
 class frontier::params {
-
+  
   case $::osfamily {
     'RedHat': {
-      $frontier_release_provider = 'rpm'
-      $frontier_release_package = 'frontier-release-1.0-1.noarch.rpm'
+      $frontier_release_provider    = 'rpm'
+      $frontier_release_package     = 'frontier-release-1.1-1.noarch.rpm'
       $frontier_release_package_url = "http://frontier.cern.ch/dist/rpms/RPMS/noarch/${frontier_release_package}"
-      $frontier_packages = ['frontier-squid']
-      $frontier_service = 'frontier-squid'
-      $frontier_customize = '/etc/squid/customize.sh'
-      $frontier_squidconf = '/etc/sysconfig/frontier-squid'
-      $frontier_cache_dir = '/var/cache/squid'
-      $resource_agents_path = '/usr/lib/ocf/resource.d/lcg'
+      $frontier_packages            = ['frontier-squid']
+      $frontier_service             = 'frontier-squid'
+      $frontier_customize           = '/etc/squid/customize.sh'
+      $frontier_squidconf           = '/etc/sysconfig/frontier-squid'
+      $frontier_cache_dir           = '/var/cache/squid'
+      $resource_agents_path         = '/usr/lib/ocf/resource.d/lcg'
     }
     default:   {
     }
-  }
-
+  }  
 }
--- manifests/squid.pp
+++ manifests/squid.pp
@@ -38,92 +38,95 @@
 # Copyright 2014 Alessandro De Salvo
 #
 class frontier::squid (
-  $customize_file = undef,
+  $customize_file     = undef,
   $customize_template = undef,
-  $cache_dir = $frontier::params::frontier_cache_dir,
-  $max_access_log = undef,
-  $install_resource = false,
-  $resource_path = $frontier::params::resource_agents_path
-) inherits params {
-  yumrepo {'cern-frontier':
-      baseurl => 'http://frontier.cern.ch/dist/rpms/',
-      enabled => 1,
+  $cache_dir          = $frontier::params::frontier_cache_dir,
+  $max_access_log     = undef,
+  $install_resource   = false,
+  $resource_path      = $frontier::params::resource_agents_path
+  )
+  inherits params {
+    
+    yumrepo {'cern-frontier':
+      baseurl  => 'http://frontier.cern.ch/dist/rpms/',
+      enabled  => 1,
       gpgcheck => 1,
       gpgkey   => 'http://frontier.cern.ch/dist/rpms/cernFrontierGpgPublicKey'
-  }
-
-  package {$frontier::params::frontier_packages:
+    }
+    
+    package {$frontier::params::frontier_packages:
       ensure  => latest,
       require => Yumrepo['cern-frontier'],
       notify  => Service[$frontier::params::frontier_service]
-  }
-
-  if ($cache_dir) {
+    }
+    
+    if ($cache_dir) {
       file { $cache_dir:
-          ensure  => directory,
-          owner   => squid,
-          group   => squid,
-          mode    => 0755,
-          require => Package[$frontier::params::frontier_packages],
-          notify  => Service[$frontier::params::frontier_service]
+        ensure  => directory,
+        owner   => squid,
+        group   => squid,
+        mode    => 0755,
+        require => Package[$frontier::params::frontier_packages],
+        notify  => Service[$frontier::params::frontier_service]
       }
-  }
-
-  if ($customize_file) {
+    }
+    
+    if ($customize_file) {
       file {$frontier::params::frontier_customize:
-          ensure  => file,
-          owner   => squid,
-          group   => squid,
-          mode    => 0755,
-          source  => $customize_file,
-          require => Package[$frontier::params::frontier_packages],
-          notify  => Service[$frontier::params::frontier_service]
+        ensure  => file,
+        owner   => squid,
+        group   => squid,
+        mode    => 0755,
+        source  => $customize_file,
+        require => Package[$frontier::params::frontier_packages],
+        notify  => Service[$frontier::params::frontier_service]
       }
-  }
-
-  if ($customize_template) {
+    }
+    
+    if ($customize_template) {
       file {$frontier::params::frontier_customize:
-          ensure  => file,
-          owner   => squid,
-          group   => squid,
-          mode    => 0755,
-          content => template($customize_template),
-          require => Package[$frontier::params::frontier_packages],
-          notify  => Service[$frontier::params::frontier_service]
+        ensure  => file,
+        owner   => squid,
+        group   => squid,
+        mode    => 0755,
+        content => template($customize_template),
+        require => Package[$frontier::params::frontier_packages],
+        notify  => Service[$frontier::params::frontier_service]
       }
-  }
-
-  if ($install_resource) {
+    }
+    
+    if ($install_resource) {
       file { $resource_path:
-          ensure  => directory,
-          owner   => "root",
-          group   => "root",
-          mode    => 0755,
+        ensure  => directory,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
       }
-
+      
       file { "${resource_path}/FrontierSquid":
-          ensure  => file,
-          owner   => "root",
-          group   => "root",
-          mode    => 0755,
-          source  => "puppet:///modules/frontier/FrontierSquid",
-          require => File[$resource_path]
+        ensure  => file,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/frontier/FrontierSquid",
+        require => File[$resource_path]
       }
-  }
-
-  file {$frontier::params::frontier_squidconf:
+    }
+    
+    file {$frontier::params::frontier_squidconf:
       ensure  => file,
       owner   => root,
       group   => root,
       mode    => 0644,
       content => template('frontier/squidconf.erb'),
       require => Package[$frontier::params::frontier_packages],
-  }
-
-  service {$frontier::params::frontier_service:
+    }
+    
+    service {$frontier::params::frontier_service:
       ensure     => running,
       enable     => true,
       hasrestart => true,
       require    => Package[$frontier::params::frontier_packages]
+    }
   }
-}
+  
